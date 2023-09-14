57e5320d213c2277d3864f7de01bee2d150e6f93
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2023-09-14 11:16:22 +0530

    Add files via upload

---
+++ Practise/whatsapp.py
@@ -0,0 +1,9 @@
+import pywhatkit
+
+# phone_number = input("Enter your number: ")
+
+# pywhatkit.sendwhatmsg(phone_number,"Test",7,15,True,2)
+
+pywhatkit.sendwhatmsg("+918809742423",
+                      "Geeks For Geeks!",
+                      23, 29)
