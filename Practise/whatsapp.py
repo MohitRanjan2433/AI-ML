d68cb4f9031836fa5e9f2ad9d5bab3aa17d2b2a9
MohitRanjan2433 <126330385+MohitRanjan2433@users.noreply.github.com>
2023-09-14 11:17:50 +0530

    Delete Practise/whatsapp.py

--- Practise/whatsapp.py
+++
@@ -1,9 +0,0 @@
-import pywhatkit
-
-# phone_number = input("Enter your number: ")
-
-# pywhatkit.sendwhatmsg(phone_number,"Test",7,15,True,2)
-
-pywhatkit.sendwhatmsg("+918809742423",
-                      "Geeks For Geeks!",
-                      23, 29)
